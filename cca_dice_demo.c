185b2fdd3792f17f4c02865ec7a62dad6ad914f3
Antonio Di Ponzio <antoniodiponzio@protonmail.com>
2026-05-26 20:06:06 +0200

    feat: added operation selector

--- cca_dice_demo.c
+++ cca_dice_demo.c
@@ -10,10 +10,23 @@
 
 #define DEVICE_PATH "/dev/realm_dice"
 
-int main(void) {
+void print_usage(const char *prog_name)
+{
+	fprintf(stdout, "Usage: %s <operation>\n", prog_name);
+	fprintf(stdout, "Available operations:\n");
+	fprintf(stdout, "  sign-rak    : Sign payload using RAK\n");
+	fprintf(stdout, "  sign-rik    : Sign payload using RIK\n");
+	fprintf(stdout, "  cert-rak    : Get RAK public key certificate\n");
+	fprintf(stdout, "  cert-rik    : Get RIK public key certificate\n");
+	fprintf(stdout, "  cert-chain  : Get machine certificate chain\n");
+}
+
+int main(int argc, char *argv[])
+{
 	int fd;
 	int ret;
-	
+	uint8_t op_code;
+
 	uint8_t hash_to_sign[64];
 	uint8_t signature_out[64];
 	
@@ -26,13 +39,30 @@ int main(void) {
 		perror("[DICE Client] Failed to open device");
 		return EXIT_FAILURE;
 	}
-	
+	/* Multiplex the op value according to argv[1] */
+	if (strcmp(argv[1], "sign-rak") == 0) {
+		op_code = RSI_DICE_OP_SIGN_RAK;
+	} else if (strcmp(argv[1], "sign-rik") == 0) {
+		op_code = RSI_DICE_OP_SIGN_RIK;
+	} else if (strcmp(argv[1], "cert-rak") == 0) {
+		op_code = RSI_DICE_OP_CERT_RAK;
+	} else if (strcmp(argv[1], "cert-rik") == 0) {
+		op_code = RSI_DICE_OP_CERT_RIK;
+	} else if (strcmp(argv[1], "cert-chain") == 0) {
+		op_code = RSI_DICE_OP_CERT_CHAIN;
+	} else {
+		fprintf(stderr, "[Error] Unknown operation: %s\n\n", argv[1]);
+		print_usage(argv[0]);
+		return EXIT_FAILURE;
+	}
+
 	// Struct defined in linux/realm_dice.h
 	struct realm_dice_sign_args args = {
 		.payload = (uintptr_t)hash_to_sign,
 		.payload_len = sizeof(hash_to_sign),
 		.signature = (uintptr_t)signature_out,
-		.sig_len = sizeof(signature_out)
+		.sig_len = sizeof(signature_out),
+		.op = opcode
 	};
 	
 	printf("[DICE Client] Sending signing request ioctl to RMM via kernel...\n");
