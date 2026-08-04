af6d2c48b6fae9cb96f902cb3b8f97589feff5f6
Antonio Di Ponzio <antoniodiponzio@protonmail.com>
2026-08-04 17:47:51 +0200

    fix: extended max certificate length

--- cca_dice_demo.h
+++ cca_dice_demo.h
@@ -1,11 +1,13 @@
 #ifndef _CCA_DICE_DEMO_H_
 #define _CCA_DICE_DEMO_H_
 
+#define MAX_CERT_LEN 1024
+
 struct cert_chain {
-	uint8_t cert_rmm[512];
+	uint8_t cert_rmm[MAX_CERT_LEN];
 	size_t cert_rmm_len;
 
-	uint8_t cert_tfa[512];
+	uint8_t cert_tfa[MAX_CERT_LEN];
 	size_t cert_tfa_len;
 };
 
